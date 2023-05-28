a29770a77d42cc96a24524087c53e89d149046d3
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-05-28 10:42:49 +0900

    stopTimer追加

--- App.tsx
+++ App.tsx
@@ -2,18 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-
 const FOCUS_TIME_MINTUTES = 0.2 * 60 * 1000;
 const BREAK_TIME_MINUTES = 0.1 * 60 + 1000;
 
-
 export default function App() {
-  const [timerCount, setTimerCount] = useState<number>(12)
+  const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINTUTES);
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null);
+
+  const startTimer = () => {
+    const id = setInterval(() => setTimerCount((prev) => prev - 1000), 1000);
+    setTimerInterval(id);
+  };
+
+  const stopTimer = () => {
+    if (timerInterval !== null) {
+      clearInterval(timerInterval);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>ぽもドーろ！</Text>
       <StatusBar style="auto" />
-      <Button title="Start timer" onPress={() => console.log("start timer")}/>
+      <Button title="Start timer" onPress={startTimer} />
+      <Button title="Stop timer" onPress={stopTimer} />
+      <Text>{timerCount}</Text>
     </View>
   );
 }
